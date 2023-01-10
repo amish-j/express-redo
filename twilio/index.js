9090bb3a92b95ed84fa24a651c6d8b2351c68906
amish-j <amishjewani6@gmail.com>
2023-01-10 12:14:29 +0530

    twilio send_sms

---
+++ twilio/index.js
@@ -0,0 +1,45 @@
+const accountSid = "AC228f4d7ae385c110e4367f019d64a175";
+const authToken = "c82f5b226c9682b343613ac131777181";
+const client = require('twilio')(accountSid, authToken);
+
+// client.messages('MM800f449d0399ed014aae2bcc0cc2f2ec')
+//       .fetch()
+//       .then(message => console.log(message.body));
+
+var send = (message,toNumber) => {
+    client.messages
+        .create({
+            body: message,
+            from: '+16516611544',
+            to: toNumber
+        })
+        .then(message => console.log(message.sid));
+}
+
+
+
+// EXAMPLE JSON RESPONSE
+// {
+//   "account_sid": "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+//   "api_version": "2010-04-01",
+//   "body": "This is the ship that made the Kessel Run in fourteen parsecs?",
+//   "date_created": "Thu, 30 Jul 2015 20:12:31 +0000",
+//   "date_sent": "Thu, 30 Jul 2015 20:12:33 +0000",
+//   "date_updated": "Thu, 30 Jul 2015 20:12:33 +0000",
+//   "direction": "outbound-api",
+//   "error_code": null,
+//   "error_message": null,
+//   "from": "+15017122661",
+//   "messaging_service_sid": "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+//   "num_media": "0",
+//   "num_segments": "1",
+//   "price": null,
+//   "price_unit": null,
+//   "sid": "SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+//   "status": "sent",
+//   "subresource_uris": {
+//     "media": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Media.json"
+//   },
+//   "to": "+15558675310",
+//   "uri": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages/SMXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json"
+// }
